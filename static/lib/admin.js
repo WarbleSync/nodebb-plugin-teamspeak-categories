8c0282ec92865859726e3679bc18c7a5d9b6dce9
WarbleSync <warblesync@gmaile.com>
2017-05-03 07:07:07 -0400

    added ts3 example

--- static/lib/admin.js
+++ static/lib/admin.js
@@ -22,22 +22,6 @@ define('admin/plugins/teamspeakcategories', ['settings'], function(Settings) {
 				});
 			});
 		});
-
-		$('#updateTS').on('click', function(){
-			Settings.save('teamspeakcategories', $('.teamspeakcategories-settings'), function() {
-				app.alert({
-					type: 'success',
-					alert_id: 'teamspeakcategories-saved',
-					title: 'Settings Saved',
-					message: 'Please refresh the page to Pair your channels/categories',
-					clickfn: function() {
-						socket.emit('admin.reload');
-					}
-				});
-			})
-
-
-		})
 	};
 
 	return ACP;
